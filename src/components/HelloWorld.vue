0c53c3b9b267b0d5642f84bfbe3a055097ce5df6
Ninomiya Yuki <84263284+Tchyarsyu@users.noreply.github.com>
2025-06-17 18:49:04 +0900

    Initialize example/todolist

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TodoList from '@/components/TodoList.vue'
+import ClickCounter from './ClickCounter.vue'
+
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <ClickCounter />
+    <TodoList />
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
